af0f592115982e919d3fd286972c5bc7eb380152
rashasharma <sharmarasha5@gmail.com>
2025-11-23 11:16:52 +0530

    file handling

--- ShoppingCart.cpp
+++ ShoppingCart.cpp
@@ -2,13 +2,14 @@
 #include <iostream>  
 #include <iomanip>
 #include <algorithm>
-void ShoppingCart::addItem(Product* item) {
+
+void ShoppingCart::addItem(std::shared_ptr<Product> item) {
     m_items.push_back(item);
 }
 
 double ShoppingCart::getTotalPrice() const {
     double total = 0.0;
-    for (Product* item : m_items) {
+    for (const auto& item : m_items) {
         total += item->getPrice();
     }
     return total;
@@ -23,15 +24,15 @@ void ShoppingCart::displayCart() const {
     std::cout << "--- Your Cart ---" << std::endl;
     std::cout << std::fixed << std::setprecision(2);
 
-    for (Product* item : m_items) {
+    for (const auto& item : m_items) {
         item->displayDetails();
     }
     
     std::cout << "-----------------" << std::endl;
     std::cout << "Total: ₹" << getTotalPrice() << std::endl;
     std::cout << "-----------------" << std::endl;
-
 }
+
 void ShoppingCart::removeItem(int id) {
     bool found = false;
 
--- ShoppingCart.h
+++ ShoppingCart.h
@@ -1,18 +1,18 @@
 #pragma once
 #include <vector>
+#include <memory>
 #include "Product.h"
-using namespace std;
 
 class ShoppingCart {
 private:
-    vector<shared_ptr<Product>> m_items;
+    std::vector<std::shared_ptr<Product>> m_items;
 
 public:
-    void addItem(shared_ptr<Product> item);
+    void addItem(std::shared_ptr<Product> item);
 
     double getTotalPrice() const;
 
     void displayCart() const;
-
+    
     void removeItem(int id);
 };
